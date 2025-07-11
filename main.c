0be412b083b2ea4618dc21b06de343c84ff8d6ce
jarregui <jarregui@student.42madrid.com>
2025-07-11 14:25:50 +0200

    juancho

--- main.c
+++ main.c
@@ -6,10 +6,15 @@
 /*   By: jarregui <jarregui@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/11 14:20:09 by jarregui          #+#    #+#             */
-/*   Updated: 2025/07/11 14:20:49 by jarregui         ###   ########.fr       */
+/*   Updated: 2025/07/11 14:25:37 by jarregui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
 // https://www.youtube.com/watch?v=yTR00r8vBH8
 // https://github.com/suspectedoceano/imperfect-cell
+
+void    main(void)
+{
+    printf("hola caracola)");
+}
