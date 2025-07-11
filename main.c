3c90c8fd35b8a92fe7b70406b7eb490bf800e2e4
jarregui <jarregui@student.42madrid.com>
2025-07-11 15:05:08 +0200

    borrado ramas

--- main.c
+++ main.c
@@ -6,14 +6,14 @@
 /*   By: jarregui <jarregui@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/11 14:20:09 by jarregui          #+#    #+#             */
-/*   Updated: 2025/07/11 15:01:37 by jarregui         ###   ########.fr       */
+/*   Updated: 2025/07/11 15:04:55 by jarregui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	main(void)
 {
 	
-	//RAMA MASTER
+	//RAMA MASTER II
 	return (0);
 }
 
