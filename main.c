902242519a767d278fc3d788404da0d2e2d36db5
jarregui <jarregui@student.42madrid.com>
2025-07-11 14:29:23 +0200

    brian

--- main.c
+++ main.c
@@ -6,10 +6,14 @@
 /*   By: jarregui <jarregui@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/11 14:20:09 by jarregui          #+#    #+#             */
-/*   Updated: 2025/07/11 14:20:49 by jarregui         ###   ########.fr       */
+/*   Updated: 2025/07/11 14:28:21 by jarregui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+int  main(void)
+ {
+    return (0);
+ }
 
 // https://www.youtube.com/watch?v=yTR00r8vBH8
 // https://github.com/suspectedoceano/imperfect-cell
